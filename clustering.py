correcting definition of kl distance with use of covariance and mean vectors, calculated pairwise kl distances between edges

clustering.py
@@ -18,18 +18,9 @@ subgraph_path = "_subgraph.gpickle"
 KL_thres = 1.0
 iters = 2
 
-
-def gaussian_pdf(x, cov, mean):
-    det = np.array([np.linalg.det(cov)]).reshape(-1,1)
-    inv = np.linalg.inv(cov)
-    norm_factor = 1 / (2 * np.pi * (det)**0.5 )
-    expo = np.exp( -0.5 * (x - mean).T.dot(inv).dot(x - mean) )
-    return norm_factor.T * expo
-
-
-def KLDistance(p1, p2):
-    KL = 2 * ( (np.where(p1 != 0, p1 * np.log(p1/p2), 0)) + (np.where(p2 != 0, p2 * np.log(p2/p1), 0)) )
-    return KL
+def KLDistance(mean1, cov1, inv1, mean2, cov2, inv2):
+    trace = np.trace((cov1 - cov2) * (inv2 - inv1))
+    return trace + (mean1 - mean2).T.dot(inv1 + inv2).dot(mean1 - mean2)
 
 
 # read in subgraph data
@@ -45,87 +36,101 @@ while os.path.isfile(path):
 
 # k-means clustering on edges with KL-distance
 num_clusters = 1
-
-sub = 1
 for subGraph in subGraphs:
-    print("subgraph ", sub)
-    sub +=1
-    for node in subGraph.nodes():
-
+    print("---------------")
+    print("SUBGRAPH")
+    print("---------------")
+    for node in subGraph.nodes(data=True):
+        print("---------------")
+        
         # convert graph attributes to arrays
-        track_state_estimates = subGraph.nodes[node]['track_state_estimates']
-        track_state_estimates_df = pd.DataFrame(track_state_estimates)
-        num_edges = subGraph.nodes[node]['degree']
+        num_edges = node[1]['degree']
+        node_state_estimates = pd.DataFrame(node[1]['track_state_estimates'])
+        edge_svs = np.vstack(node_state_estimates.loc['edge_state_vector'].to_numpy())
+        edge_covs = np.vstack(node_state_estimates.loc['edge_covariance'].to_numpy())
+        edge_covs = np.reshape(edge_covs[:, :, np.newaxis], (num_edges, 2, 2))
+        inv_covs = np.linalg.inv(edge_covs)
+        print("node num:", node)
         print("num edges:", num_edges)
 
         if num_edges <= 1: continue
-        edge_sv = np.vstack(track_state_estimates_df.loc['edge_state_vector'].to_numpy())
-        edge_cov = np.vstack(track_state_estimates_df.loc['edge_covariance'].to_numpy())
-        # adding new dimension for broadcasting
-        edge_cov = edge_cov[:, :, np.newaxis]
-        edge_cov = np.reshape(edge_cov, (num_edges, 2, 2)) 
-        mean_state_vector = subGraph.nodes[node]['mean_state_vector'].reshape(-1,1)     # column vector
-        mean_svs = np.repeat(mean_state_vector.reshape(1,-1), repeats=num_edges, axis=0)
 
-        # TODO: repeat for number of clusters
-        # initialize the center to a random candidate    
-        centre_sv = edge_sv[0].reshape(-1,1)  # column vector
-        centre_cov = edge_cov[0]
-        cluster_labels = np.empty((0, num_edges))
+        # calculate pairwise edge state vector KL distances
+        pairwise_distances = np.zeros(shape=(num_edges, num_edges))
+        for i in range(num_edges):
+            for j in range(i):
+                distance = KLDistance(edge_svs[i], edge_covs[i], inv_covs[i], edge_svs[j], edge_covs[j], inv_covs[j])
+                pairwise_distances[i][j] = distance
+                pairwise_distances[j][i] = distance
+        print("pairwise distances")
+        print(pairwise_distances)
 
-        # TODO: need to repeat the following until convergence, convergence measure?
-        n = 0
-        while n < iters:
-            print("iteration num: ", n, "\n")         
-            distances = np.empty((0, num_edges))
+        # if no outliers --> merge state vectors and covariances into a single estimate
+        # if outliers --> remove them as attributes
+        
+        print("-----------------")
 
-            for i in range(num_clusters):
-                # compute KL distance
-                p1 = gaussian_pdf(centre_sv, centre_cov, mean_state_vector)
-                p1 = np.repeat(p1, repeats=num_edges, axis=0)
-                # TODO: faster way to execute this for loop - np broadcasting?
-                p2 = np.zeros((num_edges,1))
-                for j in range(num_edges):
-                    current_p2 = gaussian_pdf(edge_sv[j], edge_cov[j], mean_svs[j])
-                    p2[j,0] = current_p2         
-                distances = KLDistance(p1, p2)
-            print("node: ", node)
-            print("distances: ", distances, "distances shape", distances.shape, "\n")
 
-            # assign each data point to the nearest cluster centre
-            cluster_labels = np.where(distances < KL_thres, 1, 0)
+        # # TODO: repeat for number of clusters
+        # # initialize the center to a random candidate    
+        # centre_sv = edge_sv[0].reshape(-1,1)  # column vector
+        # centre_cov = edge_cov[0]
+        # cluster_labels = np.empty((0, num_edges))
 
-            # recompute centre position of cluster
-            sv_cluster = edge_sv[cluster_labels[:, 0] == 1]
-            if len(sv_cluster) > 0:
-                new_centre_sv = np.mean(sv_cluster, axis=0).reshape(-1,1)
-                centre_sv = new_centre_sv
-                cov_cluster = edge_cov[cluster_labels[:, 0] == 1]
-                new_centre_cov = np.mean(cov_cluster, axis=0)
-                centre_cov = new_centre_cov
+        # # TODO: need to repeat the following until convergence, convergence measure?
+        # n = 0
+        # while n < iters:
+        #     print("iteration num: ", n, "\n")         
+        #     distances = np.empty((0, num_edges))
 
-            if (n == iters - 1):
-                print("Clustering complete after " + str(iters) + " iterations\n")
+        #     for i in range(num_clusters):
+        #         # compute KL distance
+        #         p1 = gaussian_pdf(centre_sv, centre_cov, mean_state_vector)
+        #         p1 = np.repeat(p1, repeats=num_edges, axis=0)
+        #         # TODO: faster way to execute this for loop - np broadcasting?
+        #         p2 = gaussian_pdf(edge_sv, edge_cov, mean_svs)
 
-            # repeat this until convergence
-            n += 1
+        #         # p2 = np.zeros((num_edges,1))
+        #         # for j in range(num_edges):
+        #         #     current_p2 = gaussian_pdf(edge_sv[j], edge_cov[j], mean_svs[j])
+        #         #     p2[j,0] = current_p2         
+        #         distances = KLDistance(p1, p2)
+        #     print("node: ", node)
+        #     print("distances: ", distances, "distances shape", distances.shape, "\n")
 
-        # add labels to track_state_estimates_df
-        cluster_labels = cluster_labels.reshape(1,-1)[0]
-        track_state_estimates_df.loc['cluster_labels'] = cluster_labels
-        # print("type:", type(cluster_labels[0]))
+        #     # assign each data point to the nearest cluster centre
+        #     cluster_labels = np.where(distances < KL_thres, 1, 0)
 
-        # remove edges not in cluster
-        # only run this if the length of 1's < original number of edges
-        if len(cluster_labels[cluster_labels != 0]) < num_edges:
-            print("test:")
-            print("num_edges:", num_edges)
-            print("filteres num edges:", len(cluster_labels[cluster_labels != 0]))
-            # remove outlier edges
-            # update the mean_state_vector and other attributes of the subgraph
+        #     # recompute centre position of cluster
+        #     sv_cluster = edge_sv[cluster_labels[:, 0] == 1]
+        #     if len(sv_cluster) > 0:
+        #         new_centre_sv = np.mean(sv_cluster, axis=0).reshape(-1,1)
+        #         centre_sv = new_centre_sv
+        #         cov_cluster = edge_cov[cluster_labels[:, 0] == 1]
+        #         new_centre_cov = np.mean(cov_cluster, axis=0)
+        #         centre_cov = new_centre_cov
+
+        #     if (n == iters - 1):
+        #         print("Clustering complete after " + str(iters) + " iterations\n")
+
+        #     # repeat this until convergence
+        #     n += 1
+
+        # # add labels to track_state_estimates_df
+        # cluster_labels = cluster_labels.reshape(1,-1)[0]
+        # track_state_estimates_df.loc['cluster_labels'] = cluster_labels
+        # # print("type:", type(cluster_labels[0]))
+
+        # # remove edges not in cluster
+        # # only run this if the length of 1's < original number of edges
+        # if len(cluster_labels[cluster_labels != 0]) < num_edges:
+        #     print("test:")
+        #     print("num_edges:", num_edges)
+        #     print("filteres num edges:", len(cluster_labels[cluster_labels != 0]))
+        #     # remove outlier edges
+        #     # update the mean_state_vector and other attributes of the subgraph
             
 # plot the subgraphs to view the difference after clustering
-
 
 
 # Kalman filter - fixed lag smoother

track_sim.py
@@ -16,7 +16,7 @@ from HitPairPredictor import HitPairPredictor
 
 # global variables
 DIR = "simulation/"
-threshold = 0.7  # threshold for removing nodes with edge orientation var > threshold
+threshold = 0.6  # threshold for removing nodes with edge orientation var > threshold
 
 def compute_track_state_estimates(GraphList, S):
     # computes the following node and edge attributes: vertex degree, empirical mean & variance of edge orientation
@@ -37,14 +37,8 @@ def compute_track_state_estimates(GraphList, S):
                 covariance = np.array([covariance[0,0], covariance[0,1], covariance[1,0], covariance[1,1]])
                 # print("covariance", covariance, len(covariance), type(covariance))
                 state_estimates[neighbor] = {'edge_state_vector': edge_state_vector, 'edge_covariance': covariance}
-            
             G.nodes[node]['edge_gradient_mean_var'] = (np.mean(gradients), np.var(gradients))
             G.nodes[node]['track_state_estimates'] = state_estimates
-            #compute mean state vector
-            mean_state_vector = [edge['edge_state_vector'] for edge in state_estimates.values()]
-            mean_covariance = [edge['edge_covariance'] for edge in state_estimates.values()]
-            G.nodes[node]['mean_state_vector'] = np.mean(mean_state_vector, axis=0)
-            G.nodes[node]['mean_covariance'] = np.mean(mean_covariance, axis=0)
     return GraphList
 
 
@@ -91,6 +85,15 @@ def plot_subgraphs(subGraphs, title, save=False):
     plt.axis('on')
     if save : plt.savefig(DIR + 'subgraphs.png', dpi=300)
     plt.show()
+
+    for i, s in enumerate(subGraphs):
+        _, ax = plt.subplots(figsize=(12,10))
+        pos = nx.spectral_layout(s)
+        nx.draw_networkx_nodes(s, pos)
+        nx.draw_networkx_labels(s, pos)
+        nx.draw_networkx_edges(s, pos)
+        if save : plt.savefig(DIR + 'subgraphs_visual'+ str(i) +'.png', dpi=300)
+
 
 
 def main():
@@ -183,6 +186,8 @@ def main():
     G = nx.to_directed(G)
     subGraphs = [G.subgraph(c).copy() for c in nx.weakly_connected_components(G)]
     subGraphs = compute_track_state_estimates(subGraphs, S)
+
+    # print("NUMBER of subgraphs", len(subGraphs))
     # print("first subgraph info ...")
     # for node in subGraphs[1].nodes(data=True) : print(node)
     