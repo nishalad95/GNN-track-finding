450f2af54066702074534d3de7d66a07f587303e
Nisha Nareshbhai Lad <nisha.nareshbhai.lad@cern.ch>
2022-07-20 13:28:24 +0200

    adding plotting functions to tag propagation file - checking to see what the subgraphs look like

--- tag_propagation/tag_propagation.py
+++ tag_propagation/tag_propagation.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 import json
 import pprint
@@ -93,7 +94,7 @@ print("Number of nodes after removing isolated nodes: ", endcap_graph.number_of_
 # # plot the non-isolated nodes
 # plot_subgraphs([endcap_graph], save_plot=True, node_labels=True)
 
-# FORM a dictionary keys:[values] nodes:[neighbours to process which have radius smaller than the node]
+# FORM a dictionary keys:[values] nodes:[neighbours to process which have radius larger than the node]
 # Non-parallel version
 A_nodes_to_process = A.copy()
 for node, neighbours in A.items():
@@ -102,7 +103,7 @@ for node, neighbours in A.items():
     for neighbour_node in neighbours:
         neighbour_radius = endcap_graph.nodes[neighbour_node]['zr'][1]
         # if the neighbour radius is smaller than the node radius, then remove it
-        if neighbour_radius < node_radius:
+        if neighbour_radius > node_radius:
             del A_nodes_to_process[node][neighbour_node]   
     # remove any nodes which have its number of connections to process length 0
     if len(A_nodes_to_process[node]) == 0:
@@ -131,6 +132,7 @@ num_tags_flipped = []
 frac_tags_flipped = []
 frac_flipped = 1.0
 total_number_of_nodes_to_process = len(A_nodes_to_process)
+print("total number of nodes to process: ", str(total_number_of_nodes_to_process))
 current_endcap_graph = copy.deepcopy(endcap_graph)
 while frac_flipped > threshold:
     tags_flipped = 0
@@ -143,7 +145,7 @@ while frac_flipped > threshold:
         for neighbour in connections_dict.keys():
             neighbour_tag = endcap_graph.nodes[neighbour]['tags'][-1]
             all_neighbourhood_tags.append(neighbour_tag)
-        smallest_tag = min(all_neighbourhood_tags)
+        smallest_tag = max(all_neighbourhood_tags)
         # add to the new graph - keep track of all previous tags
         current_endcap_graph.nodes[node]['tags'].append(smallest_tag)
         # get the correponding colour
@@ -163,3 +165,57 @@ while frac_flipped > threshold:
 
 print("number of tags flipped per iteration:\n", num_tags_flipped)
 print("fraction of tags flipped per iteration:\n", frac_tags_flipped)
+
+
+# plot the fraction vs iteration number
+x = np.arange(0, len(frac_tags_flipped), 1, dtype=int)
+_, ax = plt.subplots(figsize=(10,8))
+plt.scatter(x, frac_tags_flipped)
+plt.xlabel("Iteration")
+plt.ylabel("Fraction")
+plt.title("Fraction of nodes whose tags flipped value")
+plt.savefig("fraction_flipped.png", dpi=300)
+
+
+
+
+def plot_separate_subgraph(subgraphs):
+    _, ax = plt.subplots(figsize=(10,8))
+    for G in subgraphs:
+        color = G.graph['colour']
+        pos=nx.get_node_attributes(G, 'xy')
+        nodes = G.nodes()
+        nx.draw_networkx_edges(G, pos)
+        nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color=color, node_size=50)
+        nx.draw_networkx_labels(G, pos, font_size=4)
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim([-200, 200])
+    plt.ylim([-200, 200])
+    plt.axis('on')
+    plt.savefig("test_subgraph.png", dpi=300)
+    # plt.show()
+
+
+# select a few random graphs by colour to check
+all_colours = nx.get_node_attributes(current_endcap_graph, 'colour')
+unique_colours = list(set(all_colours.values()))
+print("length of all colours: ", len(all_colours))
+print("Unique colours: ", unique_colours)
+print("Number of subgraphs: ", len(unique_colours))
+
+colours = [unique_colours[0], unique_colours[1], unique_colours[2], unique_colours[10], unique_colours[20],
+            unique_colours[30], unique_colours[40], unique_colours[50], unique_colours[100], unique_colours[200]]
+print("colour to look for: ", colours)
+separate_subgraphs = []
+for colour in colours:
+    nodes_same_colour = []
+    G = nx.Graph(colour=colour)
+    for node in current_endcap_graph.nodes(data=True):
+        if node[1]['colour'] == colour:
+            G.add_node(node[0], xy=node[1]['xy'])
+    separate_subgraphs.append(G)
+
+
+plot_separate_subgraph(separate_subgraphs)
