WIP extrapolation, something wrong with the transformation - translation and rotation could be done the other way around

src/extract/extract_track_candidates.py
@@ -55,8 +55,9 @@ def get_midpoint_coords(xyzr_coords):
     return xm, ym, zm, rm
 
 
-def check_close_proximity_nodes(subgraph):
-    threshold_distance = 4.0
+def check_close_proximity_nodes(subgraph, threshold_distance):
+    # threshold_distance = 4.0        # distance between nodes used for close proximity node merging
+    print("Threshold distance for node merging: ", threshold_distance)
 
     # get the freq distribution of the vivl_ids
     vivl_id_dict = nx.get_node_attributes(subgraph, "vivl_id")
@@ -109,6 +110,7 @@ def check_close_proximity_nodes(subgraph):
                             nodes_to_merge = (node1_coords, node2_coords)
                             xm, ym, zm, rm = get_midpoint_coords(nodes_to_merge)
                             print("merging of nodes possible")
+                            print("distance separation of close proximity nodes: ", distance)
                             
                             # change subgraph attributes
                             copied_subgraph.nodes[node1]['GNN_Measurement'].x = xm
@@ -131,7 +133,7 @@ def check_close_proximity_nodes(subgraph):
                             copied_subgraph.remove_node(node2)
 
                         else:
-                            print("merging of nodes nodes not possible, too large distance between them")
+                            print("merging of nodes nodes not possible, too large distance between them, distance: ", distance)
                             copied_subgraph = None
                             break
                     else:
@@ -149,17 +151,21 @@ def check_close_proximity_nodes(subgraph):
 
 
 
-def angle_trunc(a):
-    while a < 0.0:
-        a += pi * 2
+def angle_trunc(a, p2):
+    if p2[1] > 0.0:
+        a = (2*np.pi) - a
     return a
+    # while a < 0.0:
+    #     a += pi * 2
+    # return a
 
 
 # get angle to the positive x axis in radians
 def getAngleBetweenPoints(p1, p2):
     deltaY = p2[1] - p1[1]
     deltaX = p2[0] - p1[0]
-    return angle_trunc(atan2(deltaY, deltaX))
+    angle = np.abs(atan2(deltaY, deltaX))
+    return angle_trunc(angle, p2)
 
 
 def rotate_track(coords, separation_3d_threshold):
@@ -173,9 +179,7 @@ def rotate_track(coords, separation_3d_threshold):
         p2 = coords[-3]
 
     # rotate counter clockwise, first edge to be parallel with x axis
-    angle = 2*pi - getAngleBetweenPoints(p1, p2)
-    cos_angle = np.cos(angle)
-    sin_angle = np.sin(angle)
+    angle = getAngleBetweenPoints(p1, p2)
     rotated_coords = []
     for c in coords:
         x, y, z, r = c[0], c[1], c[2], c[3]
@@ -312,6 +316,7 @@ def main():
     parser.add_argument('-f', '--fragments', help='output directory to save track fragments')
     parser.add_argument('-p', '--pval', help='chi-squared track candidate acceptance level')
     parser.add_argument('-s', '--separation_3d_threshold', help="3d distance cut between close proximity nodes, used in node merging")
+    parser.add_argument('-t', '--threshold_distance_node_merging', help="threshold_distance_node_merging")
     # parser.add_argument('-e', '--error', help="rms of track position measurements")
     parser.add_argument('-m', '--sigma_ms', help="uncertainty due to multiple scattering, process noise")
     parser.add_argument('-n', '--numhits', help="minimum number of hits for good track candidate")
@@ -328,6 +333,7 @@ def main():
     subgraph_path = "_subgraph.gpickle"
     fragment = int(args.numhits)
     separation_3d_threshold = float(args.separation_3d_threshold)
+    threshold_distance_node_merging = float(args.threshold_distance_node_merging)
     # get iteration num
     inputDir_list = inputDir.split("/")
     iterationDir = filter(lambda x: x.startswith('iteration_'), inputDir_list)
@@ -367,7 +373,7 @@ def main():
                 
                 # check for close proximity nodes based on their distance & common module_id values - merge where appropriate
                 # if merged_candidate is not None, then we need to use merged_candidate's coords in the KF, but extract the original candidate
-                candidate, merged_candidate = check_close_proximity_nodes(candidate)
+                candidate, merged_candidate = check_close_proximity_nodes(candidate, threshold_distance_node_merging)
                 
                 candidate_to_assess = candidate
                 if merged_candidate is not None: candidate_to_assess = merged_candidate
@@ -405,7 +411,7 @@ def main():
                             candidate_to_remove_from_subGraph.append(candidate)
                             
                         else:
-                            print("p-value too small, leave for further processing, pval_xy: " + pval + " pval_zr: "+ pval_zr)
+                            print("p-value too small, leave for further processing, pval_xy: " + str(pval) + " pval_zr: " + str(pval_zr))
                     else:
                         print("Candidate not accepted, not connceted in order")
 

src/extrapolate/extrapolate_merged_states.py
@@ -38,8 +38,6 @@ def extrapolate_validate(subGraph, node_num, node_attr, neighbour_num, neighbour
     # Apply both translation and rotation in order to change coordinate systems!
     x_A = (node_x * np.cos(phi)) - (node_y * np.sin(phi)) - nodeC_trans_x    # nodeA x coord in c.s. of nodeC
     y_A = (node_x * np.sin(phi)) + (node_y * np.cos(phi)) - nodeC_trans_y    # nodeA y coord in c.s. of nodeC
-    # x_A = node_x - nodeC_trans_x    # nodeA x coord in c.s. of nodeC
-    # y_A = node_y - nodeC_trans_y    # nodeA y coord in c.s. of nodeC
     print("transformed coordinates of nodeA in c.s. of nodeC (x,y): ", x_A, y_A)
 
     # calculation of track position/parameters in the target c.s. (nodeC)
@@ -47,9 +45,19 @@ def extrapolate_validate(subGraph, node_num, node_attr, neighbour_num, neighbour
     Vx_prime = np.cos(phi) + b * np.sin(phi)
     Ax_prime = a * np.sin(phi)
 
-    # s* substitution
+    # s* substitution - from solution of quadratic equation
     s_star = - ((2 * Vx_prime**2 * x_prime) + (Ax_prime * x_prime**2)) / (2 * Vx_prime**3)
     print("step size: ", s_star)
+    with open('s_star.csv', 'a') as f:
+        f.write(str(s_star) + "\n")
+    
+    # TEMPORARY checking the negative value of s* step
+    if s_star < 0.0:
+        with open('s_star_negative.csv', 'a') as f:
+            f.write(str(node_x) + " " + str(node_y) + str(neighbour_x) + " " + str(neighbour_y) + "\n" )
+    else:
+        with open('s_star_positive.csv', 'a') as f:
+            f.write(str(node_x) + " " + str(node_y) + str(neighbour_x) + " " + str(neighbour_y) + "\n" )
 
     # compute y'
     y_prime = y_A - (s_star * np.sin(phi)) + ((a*s_star**2 + b*s_star + c)*np.cos(phi))
@@ -109,7 +117,7 @@ def extrapolate_validate(subGraph, node_num, node_attr, neighbour_num, neighbour
     # calc chi2 distance between measurement at neighbour node and extrapolated track state
     H = np.array([[0., 0., 1.]])
     # residual = neighbour_y - H.dot(extrp_state)     # compute the residual
-    neighbour_y_in_cs_nodeC = .0
+    neighbour_y_in_cs_nodeC = .0                      # measurement is always zero in c.s. of neighbour
     residual = neighbour_y_in_cs_nodeC - H.dot(extrp_state)              # compute the residual
     S = H.dot(extrp_cov).dot(H.T) + sigma0**2       # covariance of residual (denominator of kalman gain)
     inv_S = np.linalg.inv(S)

src/utilities/helper.py
@@ -175,9 +175,9 @@ def compute_3d_distance(coord1, coord2):
     return np.sqrt( (x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2 )
 
 
-def angle_trunc(a):
-    while a < 0.0:
-        a += pi * 2
+def angle_trunc(a, p2):
+    if p2[1] > 0.0:
+        a = (2*np.pi) - a
     return a
 
 
@@ -185,9 +185,12 @@ def angle_trunc(a):
 def getAngleBetweenPoints(p1, p2):
     deltaY = p2[1] - p1[1]
     deltaX = p2[0] - p1[0]
-    print("initialization: ")
-    angle = angle_trunc(atan2(deltaY, deltaX))
-    print("angle: ", angle)
+    angle = np.abs(atan2(deltaY, deltaX))
+    angle = angle_trunc(angle, p2)
+    angle_deg = angle * 180/np.pi
+    print("calculating angle of rotation....")
+    print("coordinates p1 and p2: ", p1[0], p1[1], p2[0], p2[1])
+    print("angle_trunc_deg: ", angle_deg)
     return angle
 
 
@@ -202,18 +205,19 @@ def rotate_track(coords, separation_3d_threshold=None):
         if distance < separation_3d_threshold:
             p2 = coords[-3]
 
-    # rotate counter clockwise, first edge to be parallel with x axis
+    # rotate counter clockwise, first edge (origin-node edge) to be parallel with x axis
     angle = getAngleBetweenPoints(p1, p2)
-    # angle = 2*pi - getAngleBetweenPoints(p1, p2)
+    angle_deg = angle * 180/np.pi
     rotated_coords = []
     for c in coords:
         x, y = c[0], c[1]
         x_new = (x * np.cos(angle)) - (y * np.sin(angle))    # x_new = xcos(angle) - ysin(angle)
         y_new = (x * np.sin(angle)) + (y * np.cos(angle))    # y_new = xsin(angle) + ycos(angle) 
         rotated_coords.append((x_new, y_new)) 
-        print("rotate track: ")
+        print("Rotating track anticlockwise.... ")
         print("original coordinates: ", x, y)
         print("x_new, y_new:", x_new, y_new)
+        print("angle of rotation in degrees: ", angle_deg)
     return rotated_coords, angle
 
 def compute_track_state_estimates(GraphList):
@@ -254,10 +258,13 @@ def compute_track_state_estimates(GraphList):
 
             # rotate the node & its neighbours to local node-specific coordinate system
             rotated_coords, angle_of_rotation = rotate_track(coords)
+            print("all rotated coordinates: \n", rotated_coords)
+            print("angle of rotation: ", angle_of_rotation)
 
             # translate all coords such that the node in question becomes the new origin
             x_trans = rotated_coords[-2][0]
             y_trans = rotated_coords[-2][1]
+            print("x_trans, y_trans: ", x_trans, y_trans)
             transformed_coords = []
             for rc in rotated_coords:
                 tx = rc[0] - x_trans
@@ -306,6 +313,7 @@ def compute_track_state_estimates(GraphList):
             G.nodes[node]['edge_gradient_mean_var'] = (np.mean(gradients), np.var(gradients))
             # store the transformation information - used in extrapolation
             G.nodes[node]['angle_of_rotation'] = angle_of_rotation
+            print("storing x_trans, y_trans: ", x_trans, y_trans)
             G.nodes[node]['translation'] = (x_trans, y_trans)
 
     return GraphList
@@ -484,7 +492,14 @@ def __plot_save_subgraphs_iterations_in_plane(GraphList, extracted_pvals, extrac
                                                 title, key, axis1, axis2, node_labels, save_plot):
 
     _, ax = plt.subplots(figsize=(12,10))
+    iteration_1=[]
+    other_iterations=[]
     for subGraph in GraphList:
+        iteration = int(subGraph.graph["iteration"])
+        if iteration == 1: iteration_1.append(subGraph)
+        else: other_iterations.append(subGraph)
+
+    for subGraph in iteration_1:
         iteration = int(subGraph.graph["iteration"])
         color = subGraph.graph["color"]
         pos=nx.get_node_attributes(subGraph, key)
@@ -496,6 +511,20 @@ def __plot_save_subgraphs_iterations_in_plane(GraphList, extracted_pvals, extrac
         nx.draw_networkx_nodes(subGraph, pos, node_color=color, node_size=50, label=iteration)
         if node_labels:
             nx.draw_networkx_labels(subGraph, pos, font_size=4)
+    
+    for subGraph in other_iterations:
+        iteration = int(subGraph.graph["iteration"])
+        color = subGraph.graph["color"]
+        pos=nx.get_node_attributes(subGraph, key)
+        edge_colors = []
+        for u, v in subGraph.edges():
+            if subGraph[u][v]['activated'] == 1: edge_colors.append(color)
+            else: edge_colors.append("#f2f2f2")
+        nx.draw_networkx_edges(subGraph, pos, edge_color=edge_colors, alpha=0.75)
+        nx.draw_networkx_nodes(subGraph, pos, node_color=color, node_size=50, label=iteration)
+        if node_labels:
+            nx.draw_networkx_labels(subGraph, pos, font_size=4)
+    
     ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
     plt.xlabel(axis1)
     plt.ylabel(axis2)